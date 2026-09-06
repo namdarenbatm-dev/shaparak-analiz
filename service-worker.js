1456e2838794941c2b92b9af462167099d461681
namdarenbatm-dev <namdar.enbatm@gmail.com>
2026-09-06 22:44:10 +0330

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,12 @@
+const CACHE_NAME = 'analiz-shaparak-v1';
+const APP_SHELL = ['./', './index.html', './manifest.webmanifest'];
+self.addEventListener('install', event => event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting())));
+self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request).then(response => {
+    const copy = response.clone();
+    if (new URL(event.request.url).origin === self.location.origin) caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+    return response;
+  }).catch(() => caches.match('./index.html'))));
+});
